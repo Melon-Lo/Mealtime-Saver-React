7d8cce00bd52afc8d5566c517c4e8ec4d9ef3fff
Melon-Lo <dp98062@gmail.com>
2024-01-26 11:56:36 +0800

    feat/ routers setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import './App.css';
 
 // import dependencies
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
 
 // import components
 import MainPage from '../src/pages/MainPage'
 
+const basename = process.env.PUBLIC_URL
+
 function App() {
   return (
-    <BrowserRouter>
-      <MainPage />
+    <BrowserRouter basename={basename}>
+      <Routes>
+        <Route path='main' element={<MainPage />}></Route>
+        <Route path='*' element={<Navigate to="main" />}></Route>
+      </Routes>
     </BrowserRouter>
   );
 }
